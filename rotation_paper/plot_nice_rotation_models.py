075ee37b11880cbbbc47d8c3d47154d11505e680
frenchd24 <frenchd24@gmail.com>
2018-08-02 16:22:00 -0500

    made a fancy new rotating halo model gif, partway through that section of the talk

--- rotation_paper/plot_nice_rotation_models.py
+++ rotation_paper/plot_nice_rotation_models.py
@@ -145,8 +145,12 @@ def main():
 
 
     # which thing to plot?
-    plot_velocity = True
+    plot_velocity = False
     plot_3Dmodel = False
+    plot_3Dmodel_movie = True
+    
+    movie_directory = '/Users/frenchd/Research/inclination/git_inclination/thesis/DMF_thesis/NGC3633_movie/'
+
 
 
     if plot_velocity:
@@ -349,6 +353,178 @@ def main():
         tight_layout()
 
         savefig('{0}/NGC3633-RX_J1121.2+0326_3Dmodel_plot4.pdf'.format(directory),format='pdf',bbox_inches='tight')
+
+
+
+    # plot the model figure cylinder thing as a rotating movie
+    if plot_3Dmodel_movie:
+        steps = 100
+    
+        for i in arange(steps):
+            i +=1
+            
+            color_blue = '#436bad'      # french blue
+            color_red = '#ec2d01'     # tomato red
+
+            color_purple = '#7570b3'
+            color_purple2 = '#984ea3'
+            color_purple3 = '#7570b3'
+            color_purple4 = '#810f7c'
+
+            color_green = '#1b9e77'
+            color_orange = '#d95f02'
+            color_pink = '#e7298a'
+            color_lime = '#66a61e'
+            color_yellow = '#e6ab02'
+            color_brown = '#a6761d'
+            color_coal = '#666666'
+        
+        
+            # first get the data
+            z = NFW_model['z_sightline']
+            y = NFW_model['y_sightline']
+            x = NFW_model['x_sightline']
+        
+            len_step = len(z)/steps
+    
+            # now the cylinder
+            intersect = NFW_model['intersect']
+            R = NFW_model['R']
+            p0 = NFW_model['p0']
+            p1 = NFW_model['p1']
+
+            fig = plt.figure(figsize=(7.0,8.7))
+            ax = fig.add_subplot(1,1,1,projection='3d')
+        
+    
+            print 'x: ',x
+            print 'y: ',y
+            print 'z: ',z
+            print
+        
+            plotExtent = 600
+        
+            # plot the sightline
+#             ax.plot(x, y, z, color='black', alpha = 0.6, lw=2)
+            ax.plot(x[:i*len_step], y[:i*len_step], z[:i*len_step], color='black', alpha = 0.7, lw=4)
+
+            # intersect point
+            print 'intersect: ',intersect[0],intersect[1],intersect[2]
+
+        #     ax.plot([0,v[0]], [0,v[1]], [0,v[2]], color='green',lw=plotExtent/100)
+        #     ax.plot([0,v_90[0]], [0,v_90[1]], [0,v_90[2]], color='purple',lw=plotExtent/100)
+        #     ax.plot([intersect[0],v_90[0]], [intersect[1],v_90[1]], [intersect[2],v_90[2]], color='purple',lw=plotExtent/100)
+
+
+            # put a star on the intersect
+        #         planePoint_end = [-1.18639357e-01, 6.80095455e+02, -2.46470324e+02]
+        #         planePoint_end2 = [1.18630006e-01, -6.79357841e+02, 2.48330210e+02]
+        #         ax.plot([planePoint_end[0]],[planePoint_end[1]],[planePoint_end[2]],color='red',marker='*',lw=0)
+        #         ax.plot([planePoint_end2[0]],[planePoint_end2[1]],[planePoint_end2[2]],color='green',marker='*',lw=0)
+
+            
+            ax.plot([intersect[0]],[intersect[1]],[intersect[2]], color=color_yellow, marker='*', lw=0)
+        
+        ##########################################################################################
+        ##########################################################################################
+            # plot the cylinder
+
+            tube,bottom,top = plot_cylinder(p0,p1,R)
+
+            X, Y, Z = tube
+            X2, Y2, Z2 = bottom
+            X3, Y3, Z3 = top
+
+            alphaTube = 0.15
+            alphaBottom = 0.8
+            alphaTop = 0.9   
+
+#             colorTube = 'blue'
+#             colorBottom = 'red'
+#             colorTop = 'blue'
+    
+            colorTube = color_purple3
+            
+            colorBottom = color_red
+            colorTop = color_blue
+
+
+        #     ax=plt.subplot(111, projection='3d')
+            ax.plot_surface(X, Y, Z, color=colorTube, alpha = alphaTube)
+            ax.plot_surface(X2, Y2, Z2, color=colorBottom, alpha = alphaBottom)
+            ax.plot_surface(X3, Y3, Z3, color=colorTop, alpha = alphaTop)
+    
+            ax.set_xlim(-plotExtent, plotExtent)
+            ax.set_ylim(-plotExtent, plotExtent)
+            ax.set_zlim(-plotExtent, plotExtent)
+
+            # reverse the RA axis so negative is on the right
+        #     ax = plt.gca()
+            ax.invert_xaxis()
+
+            # rotate the plot
+        #         ax.view_init(elev=10., azim=5)
+        #         ax.view_init(elev=15., azim=20)
+#             ax.view_init(elev=5., azim=5)
+
+            
+            ax.view_init(elev=10., azim=3.6*i)
+
+
+            yticks((-600, -400, -200, 0, 200, 400, 600), (600, 400, 200, 0, -200, -400, -600))
+    #         xticks((-600, -400, -200, 0, 200, 400, 600), (-600, -400, -200, 0, 200, 400, 600))
+            xticks((-600, -300, 0, 300, 600), (-600, -300, 0, 300, 600))
+
+    
+            [t.set_va('center') for t in ax.get_yticklabels()]
+            [t.set_ha('center') for t in ax.get_yticklabels()]
+    
+            [t.set_va('bottom') for t in ax.get_xticklabels()]
+            [t.set_ha('left') for t in ax.get_xticklabels()]
+    
+            [t.set_va('center') for t in ax.get_zticklabels()]
+            [t.set_ha('right') for t in ax.get_zticklabels()]
+    
+            ax.grid(True)
+            ax.xaxis.pane.set_edgecolor('black')
+            ax.yaxis.pane.set_edgecolor('black')
+            ax.xaxis.pane.fill = False
+            ax.yaxis.pane.fill = False
+            ax.zaxis.pane.fill = False
+
+            ax.xaxis._axinfo['tick']['inward_factor'] = 0
+            ax.xaxis._axinfo['tick']['outward_factor'] = 0.6
+            ax.yaxis._axinfo['tick']['inward_factor'] = 0
+            ax.yaxis._axinfo['tick']['outward_factor'] = 0.6
+            ax.zaxis._axinfo['tick']['inward_factor'] = 0
+            ax.zaxis._axinfo['tick']['outward_factor'] = 0.6
+            ax.zaxis._axinfo['tick']['outward_factor'] = 0.6
+    
+            ax.set_xlabel(r'$\rm z ~ [kpc]$')
+            ax.set_ylabel(r'$\rm R.A.~ [kpc]$')
+            ax.set_zlabel(r'$\rm Dec.~ [kpc]$')
+    
+            x_label = r'$\rm z ~ [kpc]$'
+            y_label = r'$\rm R.A.~ [kpc]$'
+            z_label = r'$\rm Dec.~ [kpc]$'
+    
+            z_label = 'Dec. [kpc]'
+        #         z_label = 'abcde'
+    
+            ax.set_zlabel(z_label, rotation=0, fontsize=14, labelpad=40)
+        #         ax.set_xlabel(x_label, rotation=0, fontsize=14, labelpad=40)
+        #         ax.set_ylabel(y_label, rotation=0, fontsize=14, labelpad=40)
+
+        #         ax.xaxis.set_label_coords(10.0, -200.02)
+    
+            ax.xaxis.labelpad=18.0
+            ax.yaxis.labelpad=1.0
+            ax.zaxis.labelpad=28.0
+
+            tight_layout()
+
+            savefig('{0}/{1}.jpg'.format(movie_directory, i), dpi=200, format='jpg', bbox_inches='tight')
+
 
 
     
